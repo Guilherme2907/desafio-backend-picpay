222e17b58bfa0c902b171c5738f8ed667871d47f
Guilherme2907 <guilherme.magalhaes2907@gmail.com>
2025-07-23 22:39:12 -0300

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -90,7 +90,7 @@ resource "azurerm_service_plan" "plan" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  os_tye              = "Linux"
+  os_type              = "Linux"
   reserved            = true # Necessário para containers Linux
   sku_name		      = "B1" # Plano de baixo custo para desenvolvimento
 }
